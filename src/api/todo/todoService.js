f11c8dc1b6a89acb4086a3d44d16aebf39cef621
Nathanael Reges <nathanaelregesdev@gmail.com>
2018-07-27 17:01:31 -0300

    DataBase Working

--- src/api/todo/todoService.js
+++ src/api/todo/todoService.js
@@ -1,7 +1,24 @@
 const Todo = require('./todo')
 
 
-Todo.methods(['get', 'post', 'update' , 'delete'])
+Todo.methods(['get', 'post', 'put' , 'delete'])
 Todo.updateOptions({ new: true, runValidators: true})
+
+Todo.route('res', function(req, res, next) {
+   console.log(req.body)
+   res.send('I have a recommendation for you!');
+});
+
+Todo.before('delete', (req, res, next) => {
+   if(req.body.pass === '123') {
+      next()
+      res.send('DELETED')
+   }
+   else {
+      res.send('YOU CANT DO THAT')
+   }
+})
+
+
 
 module.exports = Todo
--- src/config/routes.js
+++ src/config/routes.js
@@ -6,7 +6,6 @@ module.exports = function (server) {
    const router = express.Router()
    server.use('/api', router)
 
-
    //TODO ROUTES
    const todoService = require('../api/todo/todoService')
    todoService.register(router, '/todos')
