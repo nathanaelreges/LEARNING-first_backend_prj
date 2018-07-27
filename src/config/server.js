79de1528861b460fd1e4ebf9d2f938a8a1e4701b
Nathanael Reges <nathanaelregesdev@gmail.com>
2018-07-27 19:20:25 -0300

    Setting Cors

--- src/config/server.js
+++ src/config/server.js
@@ -2,12 +2,13 @@ const port = 8080
 
 const bodyParser = require('body-parser')
 const express = require('express')
-
+const cors = require('./cors')
 const server = express()
 
 
 server.use(bodyParser.urlencoded( {extended: true} ))
 server.use(bodyParser.json())
+server.use(cors)
 
 
 server.listen(port, () => {
